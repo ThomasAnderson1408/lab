d4652f213335ef131a1eb8e7dbad605fed3fa69f
Jack <jeka151516@gmail.com>
2016-03-09 13:40:18 +0300

    summ+

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int a,b;
+int main(){
+    printf("Vvedite A \n");
+    scanf("%d", &a);
+    printf(Vvedite B \n);
+    scanf("%d", &b);
+    printf("%d + %d = $d", a,b,a+b);
+
+    return 0;
+}
